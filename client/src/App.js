a165ae3e0a5ada898ea795189f796daed400391a
ivan <ivanalfarogaler@gmail.com>
2024-04-14 20:59:41 +0200

    Primer commit errores

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React, { Suspense } from "react";
+import React from "react";
+import Header from './components/Header';
+import Footer from './components/Footer';
 import { Link, Route, Switch } from "wouter";
 
 import SignIn from './pages/SignIn';
 
-const HomePage = React.lazy(() => import("./pages/SignIn"));
+const HomePage = React.lazy(() => import("./pages/Home"));
 
 export default function App() {
   return (
-    <div className="App">
-      <Route component={SignIn} path="/" />
-    </div>
+
+    <section className="App-content">
+      <Header />
+      <Link to="/">
+      </Link>
+      <Switch>
+        <Route component={HomePage} path="/" />
+        <Route component={SignIn} path="/SignIn" />
+      </Switch>
+      <Footer />
+    </section>
   );
 }
---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Container} from 'react-bootstrap';
+
+function HomePage() {
+  return (
+    <div>
+        <Container>
+          <h1>Welcome to My Website</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+          </p>
+        </Container>
+    </div>
+  );
+}
+
+export default HomePage;
